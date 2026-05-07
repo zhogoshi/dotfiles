99fa8964d7fd4f9c38d55fef85afd005f1dbeb20
hogoshi <hogoshi@yandex.ru>
2026-05-07 15:30:19 +0700

    sleep

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+sleep 5
 echo "Downloading setup-nixos.sh..."
 curl -fL https://raw.githubusercontent.com/zhogoshi/dotfiles/main/setup-nixos.sh -o setup-nixos.sh
 chmod +x setup-nixos.sh
